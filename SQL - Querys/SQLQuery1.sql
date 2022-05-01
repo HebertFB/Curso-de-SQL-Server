e1bd13b05b7a62a1caf7c900f9408f26e78c9240
Hebert Farias <hebertfbarbosa@outlook.com>
2022-04-30 23:24:24 -0300

    Iniciando estudos no BD SQL Server

---
+++ SQL - Querys/SQLQuery1.sql
@@ -0,0 +1,15 @@
+--AULA 2 - SELECT + DESAFIOS aos 17:45 do video
+	--(+ "DISTINCT + DESAFIOS" aos 19:05 do video)
+
+SELECT FirstName
+	FROM Person.Person;
+
+--DISTINCT TIRAR DADOS REPETIDOS
+SELECT DISTINCT FirstName
+	FROM Person.Person;
+	
+SELECT LastName 
+	FROM Person.Person;
+
+SELECT DISTINCT LastName 
+	FROM Person.Person;
